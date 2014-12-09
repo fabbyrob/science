ac109a7080302ceb2c3f28bf68783ca67ee516b5
robert <williamson.robert.j@gmail.com>
2014-12-09 17:18:32 -0500

    adding final version of gitUpdate

--- gitUpdate.bash
+++ gitUpdate.bash
@@ -8,10 +8,7 @@ echo "pulling..."
 git pull 
 echo "adding..."
 git add $2 #add the file
-echo "committing..."
-while [ -n "$3" ]; do
-	message = "$message $3"
-	shift
-done
-git commit -m \'$message\' #commit changes, with a comment
-git push origin master
+echo "committing..."i
+echo $3
+git commit -m "$3" #commit changes, with a comment
+#git push origin master
