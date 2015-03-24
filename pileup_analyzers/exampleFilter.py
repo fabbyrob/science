d5b0c1d68070d68574f2d07c96ade9a36ab9826c
Robert Williamson <fabby@Roberts-MacBook-Air.local>
2015-03-24 14:31:56 -0400

    modified vcfSummarizer.py to use external filters, added an example filter for people to modify

---
+++ pileup_analyzers/exampleFilter.py
@@ -0,0 +1,187 @@
+import vcf
+import sys
+import argparse
+
+#process the input arguments I want to use for my filter
+#note that these arguments should NOT be named the same as arguments
+#for vcfSummaraizer.py
+#however you can also use any of those arguments, so you don't need to 
+#add a new option for min depth, for example (its -d)
+#these first few are required options, they are the defaults from vcfSummarizer.py
+#add yours after the break
+#you can see how argpars works here: https://docs.python.org/2/howto/argparse.html
+parser = argparse.ArgumentParser(description="This is my filter for VCFs it filters based on ...") #fill in the ...
+parser.add_argument("myVCF", type = str, help = "the input VCF to process")
+parser.add_argument("myAnnotation", type = str, help = "the Annotation file to use to assign site types")
+parser.add_argument("-f", "--filter", type = str, help = "the name of the script containing the filter() function")
+parser.add_argument("-d", "--min-depth", type = int, default = 20, help = "the minimum depth")
+parser.add_argument("-D", "--max-depth", type = int, default = 60, help = "the maximum depth")
+parser.add_argument("-L", "--qual", type = int, default = 40, help = "the minimum GQ")
+parser.add_argument("-i", "--dels", type = int, default = 0.0, help = "the maximum Dels")
+parser.add_argument("-G", "--genotypes", action="store_true", help = "this flag turns on outputting of individual genotypes")
+parser.add_argument("-v", "--verbose", action="store_true", help = "this flag turns on verbose mode, so more info is printed to stderr")
+parser.add_argument("-a", "--genes", action="store_true", help = "this flag turns on outputting of gene names from the annotation")
+parser.add_argument("-t", "--divergence", type= str, default = "", help = "this option takes a pickled divergence file, and will add divergence info to the output summary")
+parser.add_argument("-n", "--haploid", action="store_true", help = "this flag turns on haploid mode, a haploid VCF is expected and haploid filters should be used")
+############ add your own arguments here ############
+
+parser.add_argument("-m", "--mapqual", type = int, default = 15, help = "The minimum map quality to call a site")#this is an example, feel free to delete it
+
+#####################################################
+args = parser.parse_args()
+sys.stderr.write("Arguments for filter: %s\n" % args)#this is just so you always have a record of what you ran
+
+#set up the filter function
+#this function always takes exactly 1 argument a vcf.Record object, containing all the info from one VCF line
+#this function MUST return exactly 4 arguments
+#ref_num is the number of reference alleles at this site, after filtering
+#    e.g. 5
+#alt_num is the number of alternate alleles at this site, after filtering
+#    e.g. 3
+#total is the total number of alleles at this site, after filtering
+#    e.g. 8
+#genos is a list of all the genotype calls after filtering, in the order that samples appear in the VCF
+#    e.g. ["H","N","A","R","R","N"]
+#    in this list H is a heterozygote
+#    R is a reference homozygote
+#    A is a alternate homozygote
+#    N is an unknown genotype (this can mean both no data or it did not pass filters)
+def filter(record):
+    #this version of the filter is really dumb, 
+    #it just checks the MQ and outputs the genotypes in the GT column
+    ref_num = 0
+    alt_num = 0
+    total = 0
+    genos = []
+    
+    if record.INFO["MQ"] < args.mapqual:
+        if args.verbose:
+            sys.stderr.write("Site at %s %s has low MQ.\n" % (record.CHROM, record.POS))
+        return (0, 0, 0, ["N"]*len(record.samples))
+    
+    for sample in record.samples:
+        if sample["GT"] == "./.":
+            genos.append("N")
+        elif sample["GT"] == "0/0":
+            genos.append("R")
+            ref_num += 2
+            total += 2
+        elif sample["GT"] == "0/1":
+            genos.append("H")
+            ref_num += 1
+            alt_num += 1
+            total += 2
+        elif sample["GT"] == "1/1":
+            genos.append("A")
+            alt_num += 2
+            total += 2
+            
+    return (ref_num, alt_num, total, genos)
+
+#this following if statement will never be true if you are using vcfSummariser.py and giving it this script
+#it will only be true if you directly run *this* script on the command line
+#     e.g. >python emptyFilter.py
+#so it is a good place to test your filter
+#give it some example Records and the expected output and it will test for you
+if __name__=="__main__":
+    from collections import OrderedDict
+    tests_passed = 0
+    tests_done = 0
+    
+    #You should modify (and add more) tests like these 3 examples
+    #I HIGHLY encourage you to write these BEFORE you write the filter
+    #If you are testing on real data, and find a line that breaks your code
+    #Add it as a test here as you are fixing your code, so that you will always know that it wont
+    #re-break in the future
+    
+    ##### TEST 1  ##### 
+    #tests that we count reference homozygotes correctly
+    #info is a dictionary form of the INFO line from the VCF
+    info = {'AC':[0], 'AF':[0.0], 'MQ0':7, 'MQ':19.58}
+    #info is an OrderedDict of info from the genotypes columns - it is an OrderedDict so that the samples stay in the right order
+    #i'm spreading this out a lot so it is easy to see what all the elements are
+    genos = OrderedDict()
+    #add these one at a time or the order will be wrong
+    genos['sample_1'] = {'name':'sample_1', 'GT':'0/0', 'DP': 20, 'GQ':63, 'PL':(0.0, 21.0, 55.0)} #the keys in this dictionary are the 'name' and all the items in the FORMAT column of the VCF
+    genos['sample_2'] = {'name':'sample_2', 'GT':'0/0', 'DP': 20, 'GQ':63, 'PL':(0.0, 21.0, 55.0)}
+    genos['sample_3'] = {'name':'sample_3', 'GT':'0/0', 'DP': 20, 'GQ':63, 'PL':(0.0, 21.0, 55.0)}            
+    
+    record_1 = vcf._Record(CHROM="scaffold_1", POS=100, ID='.', REF='A', ALT='.', QUAL='.', FILTER='.', INFO=info, FORMAT='GT:DP:GQ:PL', genotypes=genos)
+    result = filter(record_1)
+    
+    tests_done += 1
+    expected = (6, 0, 6, ["R", "R", "R"])
+    if result == expected:
+        sys.stderr.write("Test 1 passed.\n")
+        tests_passed += 1
+    else:
+        sys.stderr.write("Test 1 FAILED.\n\tExpected: %s\b\t Got: %s\n\n" % (expected, result))
+        
+    ##### TEST 2  ##### 
+    #tests that we count heterozygotes correctly
+    info = {'AC':[0], 'AF':[0.5], 'MQ0':7, 'MQ':19.58}
+    genos = OrderedDict()
+    genos['sample_1'] = {'name':'sample_1', 'GT':'0/1', 'DP': 20, 'GQ':63, 'PL':(20.0, 0.0, 55.0)} #the keys in this dictionary are the 'name' and all the items in the FORMAT column of the VCF
+    genos['sample_2'] = {'name':'sample_2', 'GT':'0/1', 'DP': 20, 'GQ':63, 'PL':(20.0, 0.0, 55.0)}
+    genos['sample_3'] = {'name':'sample_3', 'GT':'0/1', 'DP': 20, 'GQ':63, 'PL':(20.0, 0.0, 55.0)}            
+    
+    record_1 = vcf._Record(CHROM="scaffold_1", POS=100, ID='.', REF='A', ALT='T', QUAL='.', FILTER='.', INFO=info, FORMAT='GT:DP:GQ:PL', genotypes=genos)
+    result = filter(record_1)
+    
+    tests_done += 1
+    expected = (3, 3, 6, ["H", "H", "H"])
+    if result == expected:
+        sys.stderr.write("Test 2 passed.\n")
+        tests_passed += 1
+    else:
+        sys.stderr.write("Test 2 FAILED.\n\tExpected: %s\b\t Got: %s\n\n" % (expected, result))
+        
+    ##### TEST 3  ##### 
+    #tests that we count alternate homozygotes and no data correctly
+    info = {'AC':[0], 'AF':[0.5], 'MQ0':7, 'MQ':19.58}
+    genos = OrderedDict()
+    genos['sample_1'] = {'name':'sample_1', 'GT':'0/0', 'DP': 20, 'GQ':63, 'PL':(20.0, 10.1, 55.0)} #the keys in this dictionary are the 'name' and all the items in the FORMAT column of the VCF
+    genos['sample_2'] = {'name':'sample_2', 'GT':'./.', 'DP': 20, 'GQ':63, 'PL':(20.0, 50.0, 0.0)}
+    genos['sample_3'] = {'name':'sample_3', 'GT':'1/1', 'DP': 20, 'GQ':63, 'PL':(20.0, 0.0, 55.0)}            
+    
+    record_1 = vcf._Record(CHROM="scaffold_1", POS=100, ID='.', REF='A', ALT='T', QUAL='.', FILTER='.', INFO=info, FORMAT='GT:DP:GQ:PL', genotypes=genos)
+    result = filter(record_1)
+    
+    tests_done += 1
+    expected = (2, 2, 4, ["R", "N", "A"])
+    if result == expected:
+        sys.stderr.write("Test 3 passed.\n")
+        tests_passed += 1
+    else:
+        sys.stderr.write("Test 3 FAILED.\n\tExpected: %s\b\t Got: %s\n\n" % (expected, result))
+        
+    ##### TEST 3  ##### 
+    #tests that we filter on MQ correctly
+    info = {'AC':[0], 'AF':[0.5], 'MQ0':7, 'MQ':5.0}
+    genos = OrderedDict()
+    genos['sample_1'] = {'name':'sample_1', 'GT':'0/0', 'DP': 20, 'GQ':63, 'PL':(20.0, 10.1, 55.0)} #the keys in this dictionary are the 'name' and all the items in the FORMAT column of the VCF
+    genos['sample_2'] = {'name':'sample_2', 'GT':'./.', 'DP': 20, 'GQ':63, 'PL':(20.0, 50.0, 0.0)}
+    genos['sample_3'] = {'name':'sample_3', 'GT':'1/1', 'DP': 20, 'GQ':63, 'PL':(20.0, 0.0, 55.0)}            
+    
+    record_1 = vcf._Record(CHROM="scaffold_1", POS=100, ID='.', REF='A', ALT='T', QUAL='.', FILTER='.', INFO=info, FORMAT='GT:DP:GQ:PL', genotypes=genos)
+    result = filter(record_1)
+    
+    tests_done += 1
+    expected = (0, 0, 0, ["N", "N", "N"])
+    if result == expected:
+        sys.stderr.write("Test 4 passed.\n")
+        tests_passed += 1
+    else:
+        sys.stderr.write("Test 4 FAILED.\n\tExpected: %s\b\t Got: %s\n\n" % (expected, result))
+        
+        
+    ##### Summary  ##### 
+    sys.stderr.write("Summary:\n\t%s/%s tests passed.\n" % (tests_passed, tests_done))
+    if tests_passed != tests_done:
+        sys.stderr.write("\tWARNING NOT ALL YOUR TESTS PASSED!\n")
+    
+    
+    
+    
+    
+    
