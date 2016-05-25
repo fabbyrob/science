f237c8e9edae5e15f95c5ada932553445b11dcbc
fabbyrob <robert.williamson@grandiflora.eeb.utoronto.ca>
2016-05-25 15:35:49 -0400

    intron analyzer with updates to AFS helper functions to be usable by other programs

--- sandbox/foldAFS.py
+++ sandbox/foldAFS.py
@@ -1,24 +1,29 @@
 import sys
+def foldAFS(afs):
+    N = len(afs)
+    newAFS = []
 
-afs = map(float, sys.argv[1:])
+    if N % 2 == 0:
+        even = True
+    else:
+        even = False
 
-N = len(afs)
-newAFS = []
+    for i, freq in enumerate(afs):
+        if not even and i == int(N/2):
+            newAFS.append(freq)
+            break
+    
+        if even and i == int(N/2):
+            break
+    
+        newAFS.append(freq+afs[N-i-1])
+    return newAFS
+ 
+if __name__=="__main__":
+    
+    afs = map(float, sys.argv[1:])
+    folded = foldAFS(afs)
 
-if N % 2 == 0:
-    even = True
-else:
-    even = False
+    print("\t".join(map(str, folded)))
 
-for i, freq in enumerate(afs):
-    if not even and i == int(N/2):
-        newAFS.append(freq)
-        break
-    
-    if even and i == int(N/2):
-        break
-    
-    newAFS.append(freq+afs[N-i-1])
-    
-    
-print("\t".join(map(str, newAFS)))
+   
